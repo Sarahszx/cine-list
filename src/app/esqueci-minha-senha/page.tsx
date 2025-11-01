8b2c2777a184e0b257d80c46fce8630279d2ac23
DeonZ-web <ewertonandre77@gmail.com>
2025-11-01 08:50:07 -0300

    tentativa de fazer a mensagem aparecer

--- src/app/esqueci-minha-senha/page.tsx
+++ src/app/esqueci-minha-senha/page.tsx
@@ -1,10 +1,17 @@
-
+'use client'
+import { useState } from 'react';
 import Image from "next/image"
 import Link from "next/link"
 
 export default function EsqueciMinhaSenha() {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleDisplay = () => {
+    setIsVisible(!isVisible);
+    
+  };
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
+    <div style={{display: isVisible ? 'block' : 'none'}} id='main' className="w-full h-screen flex flex-col justify-center items-center">
       <div className="bg-violet-300 w-3x1 rounded-2xl P-6 flex flex-col justify-center items-center gap-4 py-6">
         <Link href="/login" > <Image src="/icon-3.png" alt="Icone" width={200} height={200} className="mb-4" /> </Link>
         <div className=" text-black flex flex-col justify-center items-center text-5xl font-semibold ">
@@ -23,9 +30,28 @@ export default function EsqueciMinhaSenha() {
         </div>
 
         <div>
-          <button className="bg-violet-800 text-white p-2 rounded-2xl hover:via-violet-950 transition text-2xl py-2 px-15  ">enviar →</button></div>
+          <button onClick={ () => toggleDisplay} id='enviar' className="bg-violet-800 text-white p-2 rounded-2xl hover:via-violet-950 transition text-2xl py-2 px-15  ">
+            {isVisible ? 'Ocultar' : 'Mostrar'} enviar →</button></div>
       </div >
 
     </div>
+    
   )
+  
 }
+
+export function msgAlert() {
+  
+    const [isVisible] = useState(false);
+    return (
+    <div  style={{display: isVisible ? 'block' : 'none'}} className="w-full h-screen flex justify-center items-center">
+      <Link href="../login/page.tsx">
+       <div className="bg-gradient-to-r from-purple-800 to-gray-700 w-85 h-45 rounded-2xl items-center flex justify-center"> <h1 className="cursor-default text-center text-white-700 font-serif"><strong>Senha alterada com sucesso</strong>
+       <button className="bg-violet-900 rounded-2xl w-55 h-12 mt-6 "> <h1 className="cursor-pointer flex justify-center text-center items-center">voltar a pagina de login</h1></button></h1>
+       </div>      
+      </Link>
+    </div>
+)
+}
+
+  
