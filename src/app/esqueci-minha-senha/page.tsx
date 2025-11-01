c8a27c62da9db9e0f9300045408eef49e4df1779
Sarahszx <sarah.silvaszx@gmail.com>
2025-10-31 21:44:45 -0300

    esqueci-minha-senha2

--- src/app/esqueci-minha-senha/page.tsx
+++ src/app/esqueci-minha-senha/page.tsx
@@ -1,27 +1,29 @@
-import { Angry } from "lucide-react"
+
 import Image from "next/image"
 import Link from "next/link"
 
 export default function EsqueciMinhaSenha() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="bg-violet-300 w-1/2 rounded-2xl P-6 flex justify-center items-center">
-        <Image src="/icon-3.png" alt="Icone" width={48} height={48} /> 
-        <div className=" text-black ">
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="bg-violet-300 w-3x1 rounded-2xl P-6 flex flex-col justify-center items-center gap-4 py-6">
+        <Link href="/login" > <Image src="/icon-3.png" alt="Icone" width={200} height={200} className="mb-4" /> </Link>
+        <div className=" text-black flex flex-col justify-center items-center text-5xl font-semibold ">
 
-          Esqueci minha senha
-          
-          </div>
-        <Angry className="h-6 w-6" />
-        <Link href="/">
-          <div className="text-gray-800"> 
-             para recuperar o acesso, insira o e-mail da sua conta e enviaremos um link para redefinir
-          </div>  
-        </Link>
-<div>
-    <label htmlFor="email:">E-mail:</label>
-    <input type="email" name="" id="email" />
-</div>
+          <h1>Esqueci minha senha</h1>
+
+        </div>
+
+        <div className="text-gray-500 flex flex-col justify-center items-center text-3xl font-normal italic max-w-2xl mx-auto text-center">
+          <p>para recuperar o acesso, insira o e-mail da sua conta e enviaremos um link para redefinir.</p>
+        </div>
+
+        <div>
+          <label htmlFor="email:" className="text-black text-2xl ml-auto " >E-mail:</label>
+          <input type="email" name="" id="email" placeholder="escreva..." className=" border border-white w-full rounded-md ml-auto " />
+        </div>
+
+        <div>
+          <button className="bg-violet-800 text-white p-2 rounded-2xl hover:via-violet-950 transition text-2xl py-2 px-15  ">enviar →</button></div>
       </div >
 
     </div>
