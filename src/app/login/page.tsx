2c1acfbe14f896329a58a4b4dcc66fe957b87392
Sarahszx <sarah.silvaszx@gmail.com>
2025-11-05 16:36:59 -0300

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,3 @@
 export default function Login() {
-  return <div>Login Page</div>;
+  return <div className="w-full h-screen flex flex-col items-center text-5xl font-semibold mt-4 mb-4 ">Pagina De login</div>;
 }
