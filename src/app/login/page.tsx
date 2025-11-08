c1cbabe01a03b59507138d717f4bdd95e19d3438
fabiofraga <fabio.fraga@vialoterias.com.br>
2025-11-08 15:42:38 -0300

    adicionar tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,37 @@
 export default function Login() {
-  return <div className="w-full h-screen flex flex-col items-center text-5xl font-semibold mt-4 mb-4 ">Pagina De login</div>;
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-[url(/bg.webp)] bg-cover bg-center bg-no-repeat shadow-md">
+      <div className="bg-violet-300 p-8 rounded-2xl w-10/12 lg:w-1/3 flex flex-col gap-8">
+        <div className="flex justify-center items-center">
+          <img src="/icon-3.png" alt="Logo" className="h-36" />
+        </div>
+        
+        <p className="text-center text-black">Utilize suas credenciais abaixo para acessar sua conta.</p>
+
+        <form className="text-black flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label className="font-bold" htmlFor="email">
+              E-mail:
+              <i className="text-red-500">*</i>
+            </label>
+            <input className="bg-white h-12 px-2 rounded-md border border-gray-500" type="email" id="email" name="email" required />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label className="font-bold" htmlFor="password">
+              Senha:
+              <i className="text-red-500">*</i>
+            </label>
+            <input className="bg-white h-12 px-2 rounded-md border border-gray-500" type="password" id="password" name="password" required />
+          </div>
+
+          <a href="#" className="text-violet-800 hover:text-violet-700 underline">Esqueceu a senha?</a>
+
+          <div className="flex justify-center">
+            <button className="h-12 text-white cursor-pointer min-w-20 p-2 rounded-sm bg-violet-800 hover:bg-violet-700" type="submit">Entrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
