f4693910b74725b3fe26eb39882c7ebe8b7d299b
DeonZ-web <ewertonandre77@gmail.com>
2025-11-08 17:17:29 -0300

    redifinir senha pronta

--- src/app/esqueci-minha-senha/page.tsx
+++ src/app/esqueci-minha-senha/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Image from "next/image"
 import Link from "next/link"
+import { ArrowLeft } from 'lucide-react';
 
 export default function EsqueciMinhaSenha() {
 
@@ -26,20 +27,26 @@ export default function EsqueciMinhaSenha() {
   return (
     <div id='main' className="w-full h-screen flex flex-col justify-center items-center">
       <div className="bg-violet-300 w-3x1 rounded-2xl P-6 flex flex-col justify-center items-center gap-4 py-6">
-        <Link href="/login" > <Image src="/icon-3.png" alt="Icone" width={200} height={200} className="mb-4" /> </Link>
+        <div className='flex relative w-full justify-center'>
+          <Link href="./login">
+            <div className='bg-violet-800 hover:bg-violet-700 flex justify-center items-center rounded-full p-1 absolute left-4'>
+              <ArrowLeft />
+            </div>
+          </Link>
+          
+          <Image src="/icon-3.png" alt="Icone" width={200} height={200} className="mb-4" />
+        </div>
         <div className=" text-black flex flex-col justify-center items-center text-5xl font-semibold ">
-
           <h1>Esqueci minha senha</h1>
-
         </div>
 
         <div className="text-gray-500 flex flex-col justify-center items-center text-3xl font-normal italic max-w-2xl mx-auto text-center">
-          <p>para recuperar o acesso, insira o e-mail da sua conta e enviaremos um link para redefinir.</p>
+          <p>Para recuperar o acesso, insira o e-mail da sua conta e enviaremos um link para redefinir.</p>
         </div>
 
         <div>
           <label htmlFor="email:" className="text-black text-2xl ml-auto " >E-mail:</label>
-          <input type="email" name="" id="email" placeholder="escreva..." className=" border border-white w-full rounded-md ml-auto " />
+          <input type="email" name="" id="email" placeholder="seu@email.com" className="text-black pl-1 border border-gray-500 w-full rounded-md ml-auto " />
         </div>
 
         <div>
