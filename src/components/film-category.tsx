fbb2d5e9b3fecf1ecb3b2b6f7480e9348d025fe1
Sarahszx <sarah.silvaszx@gmail.com>
2025-12-03 16:54:39 -0300

    adicionar carrosel

--- src/components/film-category.tsx
+++ src/components/film-category.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Film } from "@/types";
+import { MoveLeft, MoveRight } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -14,25 +15,37 @@ export default function FilmCategory({ category, films }: FilmCategoryProps) {
   const visibleFilms = films.slice(currentIndex, currentIndex + 3);
 
   function handleNext() {
-    setCurrentIndex((prev) => prev + 1);
+    if (currentIndex < films.length - 3) {
+      setCurrentIndex((prev) => prev + 1);
+    }
   }
+
   function handlePrev() {
-    setCurrentIndex((prev) => prev - 1);
+    if (currentIndex > 0) {
+      setCurrentIndex((prev) => prev - 1);
+    }
   }
-  
+
   return (
     <div className="flex flex-col gap-10 bg-violet-400 rounded p-6">
       <p className=" text-black font-bold ">{category}</p>
-
-      <div className="flex overflow-hidden gap-4">
-        {visibleFilms.map((film, index) => (
-          <div key={index} className="flex flex-col gap-2 w-72 wrap-break-word">
-            <Link href={`/filme/${film.id}`}>
-              <img className="aspect-[3/4] w-72" src={film.image} alt={film.name} />
-            </Link>
-            <p className="text-center text-black">{film.name}</p>
-          </div>
-        ))}
+      <div className="flex items-start justify-between">
+        <button onClick={handlePrev} className=" bg-zinc-900 opacity-50 h-110 w-12 hover:bg-zinc-950 cursor-pointer flex items-center justify-center">
+          <MoveLeft />
+        </button>
+        <div className="flex overflow-hidden gap-4">
+          {visibleFilms.map((film, index) => (
+            <div key={index} className="flex flex-col gap-2 w-72 wrap-break-word">
+              <Link href={`/filme/${film.id}`}>
+                <img className="aspect-[3/4] w-72" src={film.image} alt={film.name} />
+              </Link>
+              <p className="text-center text-black">{film.name}</p>
+            </div>
+          ))}
+        </div>
+        <button onClick={handleNext} className="bg-zinc-900 opacity-50 h-110 w-12 hover:bg-zinc-950 cursor-pointer flex items-center justify-center">
+          <MoveRight />
+          </button>
       </div>
     </div>
   )
